a0d620e42c183a8bd0b905f8d00ea5013fe7229c
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-11-21 11:29:53 +0100

    move PadLabel styling outside the component

--- src/components/PadLabel.jsx
+++ src/components/PadLabel.jsx
@@ -1,23 +1,22 @@
 import styled from "styled-components";
 import rotations from "../rotations.json";
 
-const PadLabel = ({ labelText, triggerKey }) => {
-  const LabelText = styled.h4`
-    width: 100%;
-    margin: 0;
-    font-family: "Amatic SC", cursive;
-    font-size: 20px;
-    transform: rotate(${rotations[triggerKey]}deg);
-    background: url(images/check.png) repeat-x 0 0px;
-    display: inline-block;
-    padding: 5px 0px;
-  `;
+const LabelText = styled.h4`
+  width: 100%;
+  margin: 0;
+  font-family: "Amatic SC", cursive;
+  font-size: 20px;
+  background: url(images/check.png) repeat-x 0 0px;
+  display: inline-block;
+  padding: 5px 0px;
+`;
 
-  return (
-    <LabelText>
-      {labelText}
-    </LabelText>
-  );
+const PadLabel = ({ labelText, triggerKey }) => {
+  const styles={
+    transform: `rotate(${rotations[triggerKey]}deg)`
+  }
+
+  return <LabelText style={styles}>{labelText}</LabelText>;
 };
 
 export default PadLabel;
