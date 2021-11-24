d3dfcc6282cb4d335bc750d512dcb113b27fe2fe
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-11-24 19:50:47 +0100

    add checks to prevent playSound from triggering incorrectly

--- src/tools/playsound.js
+++ src/tools/playsound.js
@@ -4,14 +4,14 @@ const playSound = (triggerKey) => {
   const power = useStore.getState().power;
   const sequencerIsPlaying = useStore.getState().sequencerIsPlaying;
 
-  // if (power && sequencerIsPlaying) {
-  const volume = useStore.getState().volume;
-  const pad = document.querySelector(`#pad-${triggerKey}`);
-  pad.pause();
-  pad.currentTime = 0;
-  pad.volume = volume / 100;
-  pad.play();
-  // }
+  if (power && sequencerIsPlaying) {
+    const volume = useStore.getState().volume;
+    const pad = document.querySelector(`#pad-${triggerKey}`);
+    pad.pause();
+    pad.currentTime = 0;
+    pad.volume = volume / 100;
+    pad.play();
+  }
 };
 
 export default playSound;
