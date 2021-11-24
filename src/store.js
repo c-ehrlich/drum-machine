f3a014da845931ed51bfd6b4b66f175dc541b1f8
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-11-24 15:07:57 +0100

    sort and comment store.js

--- src/store.js
+++ src/store.js
@@ -3,28 +3,20 @@ import { devtools } from "zustand/middleware";
 import getSoundBankFromTitle from "./helpers";
 import sequencerInit from "./sequencer";
 
+
+/*
+ * get necessary data for initializing the store
+ */
 const soundBankInit = getSoundBankFromTitle("nujabes");
 
 
 let useStore = (set) => ({
-  power: true,
-  togglePower: () => set((state) => ({ power: !state.power })),
-  volume: 50,
-  setVolume: (volume) => set((state) => ({ ...state, volume })),
-  display: "Drum Machine",
-  setDisplay: (display) => set((state) => ({ ...state, display })),
+  // name and file location of sounds in the sound bank
   bank: soundBankInit,
   setBank: (bank) => {
     set((state) => ({ ...state, bank: getSoundBankFromTitle(bank) }));
   },
-  sequencer: sequencerInit,
-  toggleSequencer: ({ button, step }) => {
-    set((state) => {
-      console.log(button);
-      console.log(step);
-      state.sequencer[button][step] = !state.sequencer[button][step];
-    });
-  },
+
   // TODO having the current step instead of maybe something like an array of steps means that
   // every sequencer pad gets rerendered everytime the step changes
   // (confirm this with a console log in useEffect)
@@ -32,8 +24,28 @@ let useStore = (set) => ({
   // know about the state of its location in the array?
   currentStep: null,
   setCurrentStep: (currentStep) => set((state) => ({ ...state, currentStep})),
+
+  // what the display is currently showing
+  display: "Drum Machine",
+  setDisplay: (display) => set((state) => ({ ...state, display })),
+
+  // drum machine / sequencer power
+  power: true,
+  togglePower: () => set((state) => ({ power: !state.power })),
+
+  // on/off status of all sound buttons on the sequencer
+  sequencer: sequencerInit,
+  toggleSequencer: ({ button, step }) => {
+    set((state) => {
+      state.sequencer[button][step] = !state.sequencer[button][step];
+    });
+  },
+
+  // volume
+  volume: 50,
+  setVolume: (volume) => set((state) => ({ ...state, volume })),
 });
 
-useStore = devtools(useStore);
+useStore = devtools(useStore); // TEMP - remove this in prod
 useStore = create(useStore);
 export default useStore;
