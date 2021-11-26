b3f253b55cac8cdb323f1891a2087f529d3e85c7
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-11-26 16:43:32 +0100

    create Next Bank and Previous Bank buttons

--- src/components/BankSelector.jsx
+++ src/components/BankSelector.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useStore from "../store";
 import soundBanks from "../sounds.json";
 
@@ -6,14 +6,32 @@ const BankSelector = () => {
   const bank = useStore((state) => state.bank);
   const setBank = useStore((state) => state.setBank);
   const power = useStore((state) => state.power);
+  const setDisplay = useStore((state) => state.setDisplay);
+
+  useEffect(() => {
+    setDisplay(bank.name);
+  }, [bank, setDisplay])
 
   const handleChange = (e) => {
     setBank(e.target.value);
   };
 
+  const prevBank = () => {
+    // set the current active sound bank to the previous one
+    const currentIndex = soundBanks.soundBanks.findIndex((element) => element === bank);
+    const prevIndex = currentIndex === 0 ? soundBanks.soundBanks.length - 1 : currentIndex - 1;
+    setBank(soundBanks.soundBanks[prevIndex % soundBanks.soundBanks.length].url);
+  }
+
+  const nextBank = () => {
+    // set the current active sound bank to the next one
+    const nextIndex = soundBanks.soundBanks.findIndex((element) => element === bank) + 1;
+    setBank(soundBanks.soundBanks[nextIndex % soundBanks.soundBanks.length].url);
+  }
+
   return (
     <div>
-      <label htmlFor="bank-select">Choose a Bank</label>
+      <button onClick={prevBank}>{"< Previous"}</button>
       <select
         onChange={(e) => handleChange(e)}
         defaultValue={bank.url}
@@ -25,6 +43,7 @@ const BankSelector = () => {
           return <option key={key.url} value={key.url}>{key.name}</option>
         })}
       </select>
+      <button onClick={nextBank}>{"Next >"}</button>
     </div>
   );
 };
