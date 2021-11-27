f91b82e1e7d5654b6c9a8eec54eb248993a17d41
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-11-27 18:25:33 +0100

    make display darker and inset

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -5,11 +5,17 @@ import styled from "styled-components";
 const DisplayContainer = styled.div`
   width: 100%;
   height: 100%;
-  background-color: skyblue;
+  background-color: rgba(51, 152, 219, 0.7);
   display: flex;
   align-items: center;
   justify-content: center;
   font-family: 'Press Start 2P', cursive;
+
+  border: 2px inset rgba(0, 0, 0, 0.4);
+  border-radius: 4px;
+
+  ${'' /* text */}
+  color: white;
 
   &.off {
     background-color: darkblue;
