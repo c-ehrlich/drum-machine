314a797a16559d7c20818a40acaf087a5bcb0cae
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-12-04 16:41:08 +0100

    add keyboard shortcuts to BPM

--- src/components/SequencerBPM.jsx
+++ src/components/SequencerBPM.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import useStore from "../store";
 import LabelText from "../styled";
 import styled from "styled-components";
+
+const MIN_BPM = 50;
+const MAX_BPM = 180;
 
 const StyledSequencerBPM = styled.div`
   width: 100%;
@@ -16,17 +19,32 @@ const SequencerBPM = () => {
   const sequencerBPM = useStore((state) => state.sequencerBPM);
   const setSequencerBPM = useStore((state) => state.setSequencerBPM);
 
-  const handleBPMChange = (bpm) => {
+  const handleBPMChange = useCallback((bpm) => {
     setSequencerBPM(bpm);
-    setDisplay(`${String(sequencerBPM).padStart(3, '0')} bpm`);
-  }
+    setDisplay(`${String(bpm).padStart(3, '0')} bpm`);
+  }, [setDisplay, setSequencerBPM]);
+
+  useEffect(() => {
+    const handleKeyPress = (e) => {
+      if (e.key.toUpperCase() === "N" && sequencerBPM > MIN_BPM) {
+        handleBPMChange(sequencerBPM - 1);
+      }
+      if (e.key.toUpperCase() === "M" && sequencerBPM < MAX_BPM) {
+        handleBPMChange(sequencerBPM + 1);
+      }
+    };
+    document.addEventListener("keypress", handleKeyPress);
+    return () => {
+      document.removeEventListener("keypress", handleKeyPress);
+    };
+  }, [sequencerBPM, handleBPMChange])
 
   return (
     <StyledSequencerBPM>
       <input
         type="range"
-        min="50"
-        max="180"
+        min={MIN_BPM}
+        max={MAX_BPM}
         value={sequencerBPM}
         onChange={(e) => handleBPMChange(e.target.value)}
         className="slider"
